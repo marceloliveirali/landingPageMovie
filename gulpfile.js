a66622ea6001cfc5d865ebd2d7d28ff56e3407f6
Marcelo de Oliveira Lima <marceloliveirali@gmail.com>
2024-05-16 17:56:33 -0300

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: "compressed"}))
-        .pipe(gulp.dest('./dest/css'));
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dest/js'));
+        .pipe(gulp.dest('./dist/js'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles,scripts));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
