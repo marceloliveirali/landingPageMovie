cd205b16d1b6150c242fe3fd424173f79bcd42ea
Marcelo de Oliveira Lima <marceloliveirali@gmail.com>
2024-06-06 22:27:16 -0300

    Término do cabeçalho

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+
+    let open = window.document.querySelector('.container__btn--is-hamburger');
+    let close = window.document.querySelector('.container__btn--is-close');
+    let menu = window.document.querySelector('.menu');
+
+    open.addEventListener('click', function menuShow() {
+        menu.style.display = 'block';
+        open.style.display = 'none';
+        close.style.display = 'block';
+    });
+
+    close.addEventListener('click', function menuShow() {
+        menu.style.display = 'none';
+        open.style.display = 'block';
+        close.style.display = 'none';
+    });
+
